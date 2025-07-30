64db610e97ec54be4d30be08b7826a06325959d8
jnvr1 <85894381+jnvr1@users.noreply.github.com>
2025-07-29 23:09:01 -0600

    test: verify contact links and fix header tests

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,7 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HeaderComponent]
+    }).compileComponents();
+  });
+
   it('should create', () => {
     const fixture = TestBed.createComponent(HeaderComponent);
     const component = fixture.componentInstance;
